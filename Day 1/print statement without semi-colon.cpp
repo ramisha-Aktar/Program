d009d8b907a7e05551a2ee2159df2faef2f82cea
ramisha-Aktar <ramisha.aktar05@gmail.com>
2025-04-04 12:43:55 +0530

    Add files via upload

---
+++ Day 1/print statement without semi-colon.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+	if(printf("Adamas University"))
+	return 0;
+}
---
+++ Day 1/size of processor.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	double *p;
+	printf("The size of processor is %zu bit",sizeof(p)*8);
+	return 0;
+}
